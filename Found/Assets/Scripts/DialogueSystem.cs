9fdcb00382e5c1b860c39437697bc2918a4ac5b2
Nick Jordan <nick@jorcademy.nl>
2020-06-06 13:00:35 +0200

    Unity project added

---
+++ Found/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+    public Text dialogueText;
+    public Canvas dialogueCanvas;
+    public string[] dialogues;
+    public GameObject player;
+    private Vector2 playerPosition;
+    private Player playerScript;
+    private string dialogueDisplayed;
+    private bool spacePressed;
+    private Villagers villagerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spacePressed = false;
+        dialogueCanvas.enabled = false;
+        playerScript = GameObject.Find("Player").GetComponent<Player>();
+        villagerScript = this.GetComponent<Villagers>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = player.transform.position;
+
+        dialogueText.text = dialogueDisplayed;
+
+        if (spacePressed == true)
+        {
+            dialogueCanvas.enabled = true;
+
+            dialogueDisplayed = "Let me talk!!!";
+            playerScript.playerSpeed = 0;
+            playerScript.jumpForce = 0;
+            villagerScript.speed = 0;
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                spacePressed = false;
+            }
+        }
+
+        if (Vector2.Distance(transform.position, playerPosition) < 3 && spacePressed == false)
+        {
+            dialogueDisplayed = "Press Space to talk.";
+
+            dialogueCanvas.enabled = true;
+
+            playerScript.playerSpeed = 10;
+            playerScript.jumpForce = 10f;
+            villagerScript.speed = 100f;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                spacePressed = true;
+            }
+        } else if (Vector2.Distance(transform.position, playerPosition) > 3 && spacePressed == false)
+        {
+            dialogueCanvas.enabled = false;
+        }
+    }
+}
---
+++ Found/Assets/Scripts/Villagers.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Villagers : MonoBehaviour
+{
+    // Declaring needed variables
+    public float speed;
+    private bool startWalking;
+    private float direction;
+    private float secondsToWait;
+    private Rigidbody2D Rb;
+    float randomNumberGenerated;
+    public float minimumX;
+    public float maximumX;
+    public float positionX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 100f;
+        Rb = GetComponent<Rigidbody2D>();
+        startWalking = false;
+        StartCoroutine(ToggleStartWalking());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Check if the NPC can start moving
+        if (startWalking == true)
+        {
+            Rb.velocity = new Vector2(speed * Time.deltaTime, Rb.velocity.y);
+        } else if (startWalking == false)
+        {
+            Rb.velocity = new Vector2(0, 0);
+        }
+    }
+
+    private void Update()
+    {
+        positionX = transform.position.x;
+
+        if (positionX > maximumX)
+        {
+            speed *= -1;
+        } else if (positionX < minimumX)
+        {
+            speed *= -1;
+        }
+    }
+
+    // Generating a random number
+    float GenerateRandomNumber(string option)
+    {
+        if (option == "seconds")
+        {
+            randomNumberGenerated = Random.Range(0, 8);
+        } else if (option == "direction")
+        {
+            randomNumberGenerated = Random.Range(0, 2);
+        }
+
+        return randomNumberGenerated;
+    }
+
+    IEnumerator ToggleStartWalking()
+    {
+        while (true)
+        {
+            secondsToWait = GenerateRandomNumber("seconds");
+            direction = GenerateRandomNumber("direction");
+
+            yield return new WaitForSeconds(secondsToWait);
+
+            // Initializing the direction of the movement
+            if (direction == 1)
+            {
+                speed = 100f;
+            } else
+            {
+                speed = -100f;
+            }
+
+            // Passing whether or not the object should move
+            if (startWalking == false)
+            {
+                startWalking = true;
+                break;
+            }
+            else if (startWalking == true)
+            {
+                startWalking = false;
+                break;
+            }
+        }
+
+        // Starting the coroutine again
+        StartCoroutine(ToggleStartWalking());
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Makes sure the player can't push NPC's
+        if (collision.collider.name == "Player")
+        {
+            Rb.isKinematic = true;
+        }
+
+        // Making sure that the NPC's Kylie and Brandon won't push each other forward
+        if (collision.collider.name == "Kylie" || collision.collider.name == "Michael")
+        {
+            speed *= -1;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // Makes sure the player can't push NPC's
+        if (collision.collider.name == "Player")
+        {
+            Rb.isKinematic = true;
+        }
+    }
+}

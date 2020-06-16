881518bcace82bbacf48e50e7534e5a4184097ff
Nick Jordan <nick@jorcademy.nl>
2020-06-16 19:51:25 +0200

    Finishing the MaryLevel.

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -9,18 +9,24 @@ public class ButtonManager : MonoBehaviour
     private Vector2 targetPositionInteractingObject;
     public float movementSpeed;
     public bool startMoving;
+    public float distanceDown;
 
     // Start is called before the first frame update
     void Start()
     {
         startMoving = false;
-        targetPositionInteractingObject = new Vector2(interactingObject.transform.position.x, (interactingObject.transform.position.y - 20));
+
+        // Determine the position the door should move towards when a button is pressed
+        targetPositionInteractingObject = new Vector2(interactingObject.transform.position.x, (interactingObject.transform.position.y - distanceDown));
+        
+        // Making sure that the speed is the same for all screens
         movementSpeed = movementSpeed * Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Code for opening the door
         if (startMoving == true)
         {
             interactingObject.transform.position = Vector2.MoveTowards(interactingObject.transform.position, targetPositionInteractingObject, movementSpeed);
@@ -29,6 +35,7 @@ public class ButtonManager : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // If the player touches the button, the connected door will open
         if (collision.collider.name == "Player")
         {
             startMoving = true;            

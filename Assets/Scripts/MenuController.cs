73d6336e8fbec23b9aba1f264a056621a077d7a4
Nick Jordan <nick@jorcademy.nl>
2020-06-30 22:50:15 +0200

    MainMenu added.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "StartingMenu")
+        {
+            // Starting the game when the player pressed SPACE in the Main Menu
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("Center");
+            }
+
+            // Deleting the stored memory when starting a new game
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                PlayerPrefs.DeleteAll();
+            }
+        }
+    }
+}
